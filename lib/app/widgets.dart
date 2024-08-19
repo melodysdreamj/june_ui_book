8c7c55a3ca3ddc24a6527d3861db1f69bef9db32
june lee <melodysdreamj@gmail.com>
2024-08-19 23:16:52 +0900

    `

--- lib/app/widgets.dart
+++ lib/app/widgets.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:tuple/tuple.dart';
 
+import '../../uihub/music_player_ui/main.dart' as music_player_ui;
 import '../../uihub/request_ad_permission_page_ui/main.dart' as request_ad_permission_page_ui;
 
-
 final List<Tuple2<String, Widget>> widgets = [
-  Tuple2('request_ad_permission_page_ui', request_ad_permission_page_ui.NewView()),
-  
+  Tuple2('music_player_ui', music_player_ui.NewView()),
+  Tuple2('request_ad_permission_page_ui', request_ad_permission_page_ui.NewView())
 ];

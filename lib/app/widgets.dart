77f0cc325f04b75894d834c5043efbc023f348c7
june lee <melodysdreamj@gmail.com>
2024-08-22 11:07:33 +0900

    `

--- lib/app/widgets.dart
+++ lib/app/widgets.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:tuple/tuple.dart';
 
+import '../../uihub/weather_page_ui/main.dart' as weather_page_ui;
 import '../../uihub/gmail_ui/main.dart' as gmail_ui;
 import '../../uihub/music_player_ui/main.dart' as music_player_ui;
 import '../../uihub/request_ad_permission_page_ui/main.dart' as request_ad_permission_page_ui;
 
 final List<Tuple2<String, Widget>> widgets = [
+  Tuple2('weather_page_ui', weather_page_ui.NewView()),
   Tuple2('gmail_ui', gmail_ui.NewView()),
   Tuple2('music_player_ui', music_player_ui.NewView()),
   Tuple2('request_ad_permission_page_ui', request_ad_permission_page_ui.NewView())
